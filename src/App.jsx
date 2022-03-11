08be7c3f6d02f297de3cdfe270ce086daa1f3200
Max Popsuy <maxim09p@gmail.com>
2022-03-11 13:48:05 +0200

    inited repo

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import Test from './components/Test/Test';
+
+class App extends Component {
+  render() { 
+    return <Test/>;
+  }
+}
+ 
+export default App;
